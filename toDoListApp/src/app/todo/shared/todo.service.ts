529c2139e1055ec3b0410612d66e496c8ffe1c80
RudolfKlikh <rudolfklikh@gmail.com>
2019-07-28 16:35:33 +0200

    rxjs compat , functionality

--- toDoListApp/src/app/todo/shared/todo.service.ts
+++ toDoListApp/src/app/todo/shared/todo.service.ts
@@ -1,30 +1,27 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from "@angular/fire/database";
-
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  toDoList : AngularFireList<any>;
-  constructor(private firebasedb: AngularFireDatabase) { }
+  toDoList: AngularFireList<any>;
+  constructor(private firebasedb: AngularFireDatabase) {}
 
-
-  
-   gettoDoList() : AngularFireList<any> {
+  gettoDoList(): AngularFireList<any> {
     this.toDoList = this.firebasedb.list('titles');
     return this.toDoList;
   }
 
-  addTitle(title : string) {
+  addTitle(title: string) {
     this.toDoList.push({
-      title : title,
-      isChecked : false
-    })
+      title: title,
+      isChecked: false
+    });
   }
 
-  checkOrUnCheckTitle($key : string, flag : boolean) {
-    this.toDoList.update($key, { isChecked : flag});
+  checkOrUnCheckTitle($key: string, flag: boolean) {
+    this.toDoList.update($key, { isChecked: flag });
   }
   removeTitle($key: string) {
     this.toDoList.remove($key);
--- toDoListApp/src/app/todo/todo.component.ts
+++ toDoListApp/src/app/todo/todo.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TodoService } from "./shared/todo.service";
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
+  toDoListArray : any[];
+  constructor(private toDoService : TodoService) { }
 
   ngOnInit() {
-  }
+    this.toDoService.gettoDoList().snapshotChanges().subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
+    })
 
+    this.toDoListArray.sort((a,b) => {
+      return a.isChecked - b.isChecked;
+    })
+  }
+  onAdd(itemTitle) {
+    this.toDoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
+  }
 }
